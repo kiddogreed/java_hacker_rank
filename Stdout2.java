e9d6901d325c82476b1805f24b12540dbbe99423
Domingo John Ruselle R – MBTC Contractual <t-jrdomingo@mbtc.mgc.local>
2022-08-02 17:18:11 +0800

    commit all

---
+++ Stdout2.java
@@ -0,0 +1,25 @@
+/**
+ * Stdout2
+ */
+import java.util.Scanner;
+public class Stdout2 {
+
+   public static void main(String[] args) {
+      
+      Scanner scan = new Scanner(System.in);
+        
+      int i    = scan.nextInt();
+      double d = scan.nextDouble();
+      scan.nextLine();              // gets rid of the pesky newline
+      String s = scan.nextLine();
+      scan.close();
+      
+      
+      
+      
+
+      System.out.println("String: " + s);
+      System.out.println("Double: " + d);
+      System.out.println("Int: " + i);
+   }
+}
